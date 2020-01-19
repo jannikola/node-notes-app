66eaa7e2d83e9604a647dee411625981bbe1c7c0
jannikola <jannikola@gmail.com>
2020-01-19 17:02:30 +0100

    body argument to create command added

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ yargs.version('1.1.0');
 yargs.command({
     command : 'add',
     describe : 'Add a new note',
-    handler : function () {
-        console.log('Adding a new note...');
+    builder : {
+        title : {
+            describe : 'Note title',
+            demandOption : true,
+            type : 'string',
+        },
+        body : {
+            describe : 'Note body',
+            demandOption : true,
+            type : 'string',
+        }
+    },
+    handler : function (argv) {
+        console.log('Title: ', argv.title);
+        console.log('Body: ', argv.body);
     }
 })
 
@@ -40,6 +53,6 @@ yargs.command({
     }
 })
 
-//add, remove, read, list
+yargs.parse();
 
-console.log(yargs.argv);
+//console.log(yargs.argv);
