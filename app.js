6a263ea263ee4542da8592d2409c3f191f48dcf7
jannikola <jannikola@gmail.com>
2020-01-19 15:26:49 +0100

    setup commands to suport add, remove, read, list commands

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const chalk = require('chalk');
-const validator = require('validator');
+const yargs = require('yargs');
 
-const command = process.argv[2];
+//customize yargs version 
+yargs.version('1.1.0');
 
-console.log(process.argv);
+//create add command
+yargs.command({
+    command : 'add',
+    describe : 'Add a new note',
+    handler : function () {
+        console.log('Adding a new note...');
+    }
+})
 
-if(command === 'add') {
-    console.log('adding note...');
-} else if(command === 'remove') {
-    console.log('removing note...')
-}
+//create remove command
+yargs.command({
+    command : 'remove',
+    describe : 'Remove a note',
+    handler : function () {
+        console.log('Removing the note...');
+    }
+})
+
+//create read command
+yargs.command({
+    command : 'read',
+    describe : 'Read notes',
+    handler : function () {
+        console.log('Reading notes...');
+    }
+})
+
+//create list command
+yargs.command({
+    command : 'list',
+    describe : 'List all notes',
+    handler : function () {
+        console.log('Listing notes...');
+    }
+})
+
+//add, remove, read, list
+
+console.log(yargs.argv);
